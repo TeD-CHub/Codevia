b5dc8466a1eee41791364906528bc11f4ed7a95c
Churchill-Techie <churchill4032@gmail.com>
2025-08-03 19:13:51 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Toggle dark mode
+
+// js/main.js
+document.getElementById('darkToggle').addEventListener('change', function () {
+  document.body.classList.toggle('dark-mode');
+  document.body.classList.toggle("dark");
+});
+
+
+
