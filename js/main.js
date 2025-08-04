b88e10149397da610d8882672ad5a9499a40f2ff
Churchill-Techie <churchill4032@gmail.com>
2025-08-04 12:12:24 +0300

    Add hamburger menu and style fixes

--- js/main.js
+++ js/main.js
@@ -6,5 +6,11 @@ document.getElementById('darkToggle').addEventListener('change', function () {
   document.body.classList.toggle("dark");
 });
 
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('open');
+});
 
 
